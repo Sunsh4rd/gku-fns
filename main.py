d0f7609204231c0bfcc051a822ab81d3340f896c
user <SsoslickK@gmail.com>
2023-09-15 17:04:49 +0400

    format excel

--- main.py
+++ main.py
@@ -3,22 +3,38 @@ import requests
 import openpyxl
 import os
 
+from openpyxl.styles import PatternFill
+
+
+def format_excel(file):
+    key = os.environ.get('FNS_API_KEY')
+    book = openpyxl.load_workbook(file)
+    ws = book.active
+    # TODO: uncomment when new key arrives
+    # ogrns = [cell.value for cell in ws['H']][1:]
+    # urls = [f'https://api-fns.ru/api/search?q={ogrn}&key={key}' for ogrn in ogrns]
+    # print(urls)
+    # companies = []
+    # for url in urls:
+    #     print(f'request for {url}')
+    #     company = requests.get(url).json()
+    #     print(f'got company {company}')
+    #     companies.append(company)
+    # print(companies)
+    # with open('response.json', 'w', encoding='utf-8') as response_w:
+    #     json.dump(companies, response_w, indent=4)
+    red_fill = PatternFill(start_color='FFFF0000', end_color='FFFF0000', fill_type='solid')
+    # TODO: new xlsx file stays unformatted with the if statement, but formats ok without the if statement
+    for i in range(2, ws.max_row + 1):
+        if ws[f'J{i}'].value == 'ЛИКВИДИРОВАНА':
+            ws[f'K{i}'].value = 'НЕАКТИВНО'
+            ws[f'K{i}'].fill = red_fill
+    book.save('new.xlsx')
+    book.close()
+
 
 def main():
-    key = os.environ.get('FNS_API_KEY')
-    book = openpyxl.load_workbook('выверка МИНЦИФРЫ.xlsx')
-    ogrns = [cell.value for cell in book['Саратовская область']['H']][1:]
-    urls = [f'https://api-fns.ru/api/search?q={ogrn}&key={key}' for ogrn in ogrns]
-    print(urls)
-    companies = []
-    for url in urls:
-        print(f'request for {url}')
-        company = requests.get(url).json()
-        print(f'got company {company}')
-        companies.append(company)
-    print(companies)
-    with open('response.json', 'w', encoding='utf-8') as response_w:
-        json.dump(companies, response_w, indent=4)
+    format_excel('выверка МИНЦИФРЫ.xlsx')
 
 
 if __name__ == '__main__':
